542cdbffa608e71b1182d65f54de8618c568aa6f
Kep <ryan.murphy@uni-potsdam.de>
2024-10-30 14:54:52 +0100

    Added environments

--- envs/params.py
+++ envs/params.py
@@ -1,10 +1,10 @@
 # basic parameters
 width=30
 height=30
-number_of_agents=1
-max_num_cities=2
+number_of_agents=2
+max_num_cities=4
 seed=1
-grid_mode=True
+grid_mode=False
 max_rails_between_cities=2
 max_rail_pairs_in_city=2
 remove_agents_at_target=True
@@ -13,6 +13,6 @@ remove_agents_at_target=True
 speed_ratio_map={1 : 1}
 
 # malfunctions
-malfunction_rate=1/30
+malfunction_rate=0/30
 min_duration=2
 max_duration=6
