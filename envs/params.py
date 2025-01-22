b66b71d66efb3f311ee712ba45f2fd59d70b5231
Feign Death <gord.lame@hotmail.de>
2025-01-22 17:52:43 +0100

    revert to simple instance generation

--- envs/params.py
+++ envs/params.py
@@ -1,8 +1,8 @@
 # basic parameters
 width=40
 height=40
-number_of_agents=4
-max_num_cities=4
+number_of_agents=2
+max_num_cities=2
 seed=1
 grid_mode=False
 max_rails_between_cities=2
@@ -19,6 +19,6 @@ speed_ratio_map={1 : 1}
 #     }
 
 # malfunctions
-malfunction_rate=2/30
+malfunction_rate=0/30
 min_duration=2
 max_duration=6
