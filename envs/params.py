860bd485f1af9afdeae6f330dbc347f556c0702d
Feign Death <gord.lame@hotmail.de>
2025-01-22 17:30:27 +0100

    add an example

--- envs/params.py
+++ envs/params.py
@@ -1,8 +1,8 @@
 # basic parameters
 width=40
 height=40
-number_of_agents=2
-max_num_cities=2
+number_of_agents=4
+max_num_cities=4
 seed=1
 grid_mode=False
 max_rails_between_cities=2
@@ -10,15 +10,15 @@ max_rail_pairs_in_city=2
 remove_agents_at_target=True
 
 # speed
-# speed_ratio_map={1 : 1}
-speed_ratio_map= {
-        1:   0.50,
-        1/2: 0.00,  
-        1/3: 0.00,
-        1/4: 0.50
-    }
+speed_ratio_map={1 : 1}
+# speed_ratio_map= {
+#         1:   0.50,
+#         1/2: 0.00,  
+#         1/3: 0.00,
+#         1/4: 0.50
+#     }
 
 # malfunctions
-malfunction_rate=0/30
+malfunction_rate=2/30
 min_duration=2
 max_duration=6
