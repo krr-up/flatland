16257e50f66c9edecf1d9f3f8a76843f196d9262
Feign Death <gord.lame@hotmail.de>
2025-01-23 13:40:18 +0100

    better test generation

--- envs/params.py
+++ envs/params.py
@@ -1,7 +1,7 @@
 # basic parameters
 width=40
 height=40
-number_of_agents=20
+number_of_agents=4
 max_num_cities=4
 seed=1
 grid_mode=False
@@ -19,6 +19,6 @@ speed_ratio_map={1 : 1}
 #     }
 
 # malfunctions
-malfunction_rate=0/30
+malfunction_rate=1/100
 min_duration=2
-max_duration=6
+max_duration=2
