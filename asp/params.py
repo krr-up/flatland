a919d6173eceac015240d49cb2411c49ad5b7b02
xarxaxdev <xarxax.dev@gmail.com>
2025-02-24 00:20:07 +0100

    more efficient timewise solution, added Direction to edges

--- asp/params.py
+++ asp/params.py
@@ -1,6 +1,7 @@
 # primary=['asp/malfunctions_test.lp']
 primary=['asp/graph_reschedule/0_input.lp','asp/graph_reschedule/1_path.lp','asp/graph_reschedule/2_output.lp']
-secondary=['asp/graph_rework/0_input.lp','asp/graph_rework/1_path.lp','asp/graph_rework/2_output.lp','asp/graph_rework/3_malfunction.lp']
+secondary=['asp/graph_atom_minimalistic/0_input.lp','asp/graph_atom_minimalistic/1_path.lp','asp/graph_atom_minimalistic/2_output.lp','asp/graph_atom_minimalistic/3_malfunction.lp']
+secondary=['asp/graph_atom_minimalistic_2/0_input.lp','asp/graph_atom_minimalistic/1_path.lp','asp/graph_atom_minimalistic/2_output.lp','asp/graph_atom_minimalistic/3_malfunction.lp']
 # secondary=['asp/graph_continue/2_output.lp','asp/graph_continue/3_malfunction.lp']
 # secondary=["asp/graph_reschedule/3_malfunction.lp"]
 #primary=['asp/test2.lp']
