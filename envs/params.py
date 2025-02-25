cb231416fbbbefe15a1c04e09c9d8174d91ae5ed
Feign Death <gord.lame@hotmail.de>
2025-02-25 16:25:59 +0100

    add dafault testing instances

--- envs/params.py
+++ envs/params.py
@@ -2,23 +2,17 @@
 width=24
 height=24
 number_of_agents=20
-max_num_cities=2
+max_num_cities=3
 seed=1
 grid_mode=False
-max_rails_between_cities=2
-max_rail_pairs_in_city=2
+max_rails_between_cities=1
+max_rail_pairs_in_city=1
 remove_agents_at_target=True
 
 # speed
 speed_ratio_map={1 : 1}
-# speed_ratio_map= {
-#         1:   0.50,
-#         1/2: 0.00,  
-#         1/3: 0.00,
-#         1/4: 0.50
-#     }
 
 # malfunctions
-malfunction_rate=1/100
+malfunction_rate=5/100
 min_duration=2
-max_duration=2
+max_duration=6
