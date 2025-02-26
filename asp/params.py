39dfa2282beecf3f9a989177c40e0c24e77b54b4
Feign Death <gord.lame@hotmail.de>
2025-02-26 22:48:01 +0100

    slighly better

--- asp/params.py
+++ asp/params.py
@@ -1,2 +1,3 @@
-primary=["asp/incremental/primary.lp"]
-secondary=[]
+primary = ["asp/incremental/primary.lp"]
+# primary = ["asp/graph_reschedule/0_input.lp", "asp/graph_reschedule/1_path.lp", "asp/graph_reschedule/2_output.lp"]
+secondary = ["asp/incremental/secondary.lp"]
