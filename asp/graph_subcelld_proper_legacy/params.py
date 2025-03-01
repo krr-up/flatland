e2f4479403144876f8da85fb6a0befe9c2f252b9
xarxaxdev <xarxax.dev@gmail.com>
2025-03-01 16:40:55 +0100

    corrected the issues that happened on the simulation, but not during solving

---
+++ asp/graph_subcelld_proper_legacy/params.py
@@ -0,0 +1,3 @@
+# For using this, just overwrite asp/params.py with this file
+primary=['asp/graph_subcelld_proper/0_input.lp','asp/graph_subcelld_proper/1_path.lp','asp/graph_subcelld_proper/2_output.lp']
+secondary=['asp/graph_subcelld_proper/1_path.lp','asp/graph_subcelld_proper/2_output.lp','asp/graph_subcelld_proper/3_malfunction.lp']
