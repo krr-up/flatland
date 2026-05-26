0491d891809e4550771b86549efcfa718aad4ce3
murphy2 <ryan.murphy@uni-potsdam.de>
2026-05-26 15:42:43 +0200

    For the course

--- asp/params.py
+++
@@ -1,5 +0,0 @@
-#primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
-#primary=['asp/action_based/actions.lp','asp/action_based/pathfinding.lp','asp/action_based/transitions.lp']
-
-primary=['asp/test.lp']
-secondary=[]
--- benchmarks/test.py
+++
@@ -1,3 +0,0 @@
-import sys
-import os
-print(os.path.splitext(sys.argv[1]))
