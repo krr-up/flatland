ea78d99c31fa9422dd26be1032b0a04fd852f802
murphy2 <ryan.murphy@uni-potsdam.de>
2026-06-01 15:12:14 +0200

    Updating .gitignore

--- envs/params.py
+++ envs/params.py
@@ -1,8 +1,8 @@
 # basic parameters
-width=80
-height=80
+width=30
+height=30
 number_of_agents=4
-max_num_cities=6
+max_num_cities=4
 seed=1
 grid_mode=False
 max_rails_between_cities=2
@@ -12,13 +12,13 @@ remove_agents_at_target=True
 # speed
 # speed_ratio_map={1 : 1}
 speed_ratio_map= {
-        1:   0.50,
+        1:   1.00,
         1/2: 0.00,  
         1/3: 0.00,
-        1/4: 0.50
+        1/4: 0.00
     }
 
 # malfunctions
-malfunction_rate=0/30
+malfunction_rate=0/100
 min_duration=2
 max_duration=6
